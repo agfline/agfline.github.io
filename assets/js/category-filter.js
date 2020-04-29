f1f171ed03862e66b893e4032536525daa3f50dc
Adrien Gesta-Fline <dev.agfline@posteo.net>
2020-04-29 10:13:21 +0200

    Update category-filter.js

--- assets/js/category-filter.js
+++ assets/js/category-filter.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    alert(window.location.hash);
+    var catRequest = window.location.hash.slice(1);
+
+    if ( !catRequest )
+      return;
+
+    // Each categorized group of posts is in a section.
+    // No other section in document.
+    var sections = document.querySelectorAll("section");
+    for (var i = 0; i < sections.length; i++) {
+      if (sections[i].id != catRequest)
+        sections[i].style.display = "none";
+    }
+
+    }, false);
 }, false);
