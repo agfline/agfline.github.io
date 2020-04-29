5a6c72180c487a7bcba47e2b241a563bc9573539
Adrien Gesta-Fline <dev.agfline@posteo.net>
2020-04-29 11:40:34 +0200

    Update category-filter.js

--- assets/js/category-filter.js
+++ assets/js/category-filter.js
@@ -1,11 +1,14 @@
-function filterCategory( catRequest ) {
+function filterCategory( catRequest )
+{
   if ( !catRequest )
     return;
 
   // Each categorized group of posts is in a section.
   // No other section in document.
   var sections = document.querySelectorAll("section");
-  for (var i = 0; i < sections.length; i++) {
+  
+  for (var i = 0; i < sections.length; i++)
+  {
     if (sections[i].id == catRequest)
       sections[i].style.display = "block";
     else
@@ -16,6 +19,10 @@ function filterCategory( catRequest ) {
 document.addEventListener('DOMContentLoaded', function() {
   filterCategory(window.location.hash.slice(1));
   console.log(window);
+  window.onpopstate = function() {
+    console.log("popstate");
+    filterCategory(window.location.hash.slice(1));
+  }
 }, false);
 
 document.addEventListener('hashchange', function() {
